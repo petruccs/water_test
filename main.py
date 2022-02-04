a85f6eaffde631ec18b637941a3bac653a48f91d
Stefano Petrucci <petrucci.ste@gmail.com>
2022-02-04 18:43:56 +0000

    added goals of the test + minor changes

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from parser import parser
+from dataset import dataSet
+from arima import *
+"""
+Tasks:
+- L'obiettivo dell'esercizio è di scegliere uno di questi waterbodies (puoi scegliere quello che preferisci, per es., quello con più dati), per:
 
-file_name = "data/Water_Spring_Lupa.csv"
-my_parser = parser(file_name)
+-- 1 Studiare le relazioni tra dati di input e variabile target (in generale, livello dell'acqua)
+-- 2 Fare una previsione dell'andamento della variabile target nei successivi giorni
+"""
+
+file_name = "data/Lake_Bilancino.csv"
+my_parser = dataSet(file_name)
+
+#plot_auto_corr(my_parser.df, my_parser.df.columns[-1], True)
+
+test_arima_model(my_parser.df[my_parser.df.columns[-1]], 5, 1, 0)
