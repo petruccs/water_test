24e84d84e79788f2c27b52856831855525d70b4e
Stefano Petrucci <petrucci.ste@gmail.com>
2022-02-04 18:46:49 +0000

    added preliminary functions, WIP

---
+++ autoregression.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
+
+
+def plot_auto_corr(df, column, interpolate):
+    """
+    Plots the autocorrelation function of the given column.
+    """
+    print(
+        f"[INFO] plotting the autocorrelation of {column}; interpolate = {interpolate}"
+    )
+
+    plt.figure()
+    if interpolate:
+        pd.plotting.autocorrelation_plot(df[column].interpolate())
+    else:
+        pd.plotting.autocorrelation_plot(df[column])
+
+    plt.show()
+
+
+def test_arima_model(series, p, d, q):
+    model = ARIMA(series, order=(p, d, q))
+    model_fit = model.fit()
+    print(model_fit.summary())
+    residuals = pd.DataFrame(model_fit.resid)
+    residuals.plot()
+    plt.show()
+    residuals.plot(kind='kde')
+    plt.show()
+    #pyplot.show()
+    ## summary stats of residuals
+    print(residuals.describe())
