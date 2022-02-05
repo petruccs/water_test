eaccfaeb1eef0c193b19a0c6a238adbbcf60a579
Stefano Petrucci <petrucci.ste@gmail.com>
2022-02-05 17:28:13 +0000

    comments + run predictions

--- autoregression.py
+++ autoregression.py
@@ -6,6 +6,10 @@ from statsmodels.tsa.arima.model import ARIMA
 def plot_auto_corr(df, column, interpolate):
     """
     Plots the autocorrelation function of the given column.
+    Parameters:
+        df: The dataframe containing the data.
+        column: The column to be plotted.
+        interpolate: If True, the NaN values are interpolated using the linear method.
     """
     print(
         f"[INFO] plotting the autocorrelation of {column}; interpolate = {interpolate}"
@@ -16,11 +20,19 @@ def plot_auto_corr(df, column, interpolate):
         pd.plotting.autocorrelation_plot(df[column].interpolate())
     else:
         pd.plotting.autocorrelation_plot(df[column])
-
+    plt.title(f"Autocorrelation of {column}")
     plt.show()
 
 
 def test_arima_model(series, p, d, q):
+    """
+    Tests the ARIMA model with the given parameters.
+    Parameters:
+        series: The series to be tested.
+        p: Lag
+        d: 
+        q: 
+    """
     model = ARIMA(series, order=(p, d, q))
     model_fit = model.fit()
     print(model_fit.summary())
@@ -30,3 +42,24 @@ def test_arima_model(series, p, d, q):
     residuals.plot(kind='kde')
     plt.show()
     print(residuals.describe())
+
+
+def run_arima_prediction(series, p, d, q, n_pred):
+    """
+    Runs the ARIMA model with the given parameters.
+    Parameters:
+        series: The series to be tested.
+        p: Lag
+        d: 
+        q: 
+    """
+    #res_seq = []
+    #for i in range(n_pred):
+    #    print(i)
+    #    model = ARIMA(series, order=(p, d, q))
+    #    model_fit = model.fit()
+    #    #res_seq.append(model_fit.forecast()[0])
+    #    print(model_fit.forecast())
+    model = ARIMA(series, order=(p, d, q))
+    model_fit = model.fit()
+    return model_fit.forecast(n_pred)
